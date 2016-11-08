b50a7a8b9e55b6e46379970c6d1e37308c5515fb
deeforabroad <dee.forabroad@gmail.com>
2016-11-08 15:09:46 +0100

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author dee
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		TestName test = new TestName();
+		test.setName("Test GitHub");
+		System.out.println(test.getName());
+	}
+
+}
---
+++ src/TestName.java
@@ -0,0 +1,13 @@
+
+public class TestName {
+	private String _name;
+	public TestName() {
+	
+	}
+	public void setName(String value) {
+		_name = value;
+	}
+	public String getName() {
+		return _name;
+	}
+}
